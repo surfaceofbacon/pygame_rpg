4aede74ab05817b100c30c368c3b8fa977de0c6f
surfaceofbacon <rcindustries17@gmail.com>
2023-04-20 18:00:31 -0700

    this is a test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 swordName = input('Please enter a sword name:')
+armorName = input('Please enter an armor name:')
 class sword:
     defence = 10
     damage = 3
     xp = 7
 print(swordName)
+print(armorName)
