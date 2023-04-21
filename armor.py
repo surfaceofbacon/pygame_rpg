791122c08949a5f47ee72e7b3a95f969d50ba57e
surfaceofbacon <rcindustries17@gmail.com>
2023-04-20 18:17:58 -0700

    this is a test

---
+++ armor.py
@@ -0,0 +1,14 @@
+def main():
+    armorName = input('Please enter an armor name:')
+    print(armorName)
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import armor
 swordName = input('Please enter a sword name:')
-armorName = input('Please enter an armor name:')
-class sword:
-    defence = 10
-    damage = 3
-    xp = 7
+
 print(swordName)
-print(armorName)
+armor.main()
