b49d8da55301a3aa1179f29dc407971ffb28c79f
surfaceofbacon <rcindustries17@gmail.com>
2023-04-22 21:33:14 -0700

    this is a test

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-import armor
-swordName = input('Please enter a sword name:')
+import pygame
 
-print(swordName)
-armor.main()
+pygame.init()
+
+size = 1280, 720
+
+run = True
+
+white = 255, 255, 255
+
+window = pygame.display.set_mode(size)
+
+
+def draw_window():
+    window.fill(white)
+    pygame.display.update()
+
+
+def main():
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        draw_window()
+
+
+if __name__ == '__main__':
+    main()
+
