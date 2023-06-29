b161c32a5c26aa6f98fcf9e35caa3e77c068d337
surfaceofbacon <rcindustries17@gmail.com>
2023-06-29 19:35:13 -0400

    this is a test

--- enemies.py
+++ enemies.py
@@ -1,3 +1,15 @@
-class skeleton:
-    AC = 5
-    health = 10
+import attack
+
+
+class Enemy:
+
+    def __init__(self, AC, health):
+        self.AC = AC
+        self.health = health
+
+
+    def attack(self, weapon):
+        attack.attack(weapon, enemy = player)
+
+
+skeleton = Enemy(15, 10)
