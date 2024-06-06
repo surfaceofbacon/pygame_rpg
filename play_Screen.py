50244eca4cd4ff9cbdfbb9c6431f8561d92607f7
surfaceofbacon <rcindustries17@gmail.com>
2024-06-05 20:07:24 -0400

    added random map "roguelike" functionality

--- play_Screen.py
+++ play_Screen.py
@@ -2,6 +2,7 @@ import Draw_text
 import window
 import pygame
 import menu
+import random
 black = (0,0,0)
 white = (255,255,255)
 red = (255, 0, 0)
@@ -13,7 +14,7 @@ magenta = (255, 0, 255)
 gray = (128, 128, 128)
 background = pygame.image.load('testmap.png')
 levels = [black, white, red, green, blue, yellow, cyan, magenta, gray]
-
+random.shuffle(levels)
 
 def Draw_Play_Screen(number):
     #window.window.blit(background, (0,0))
